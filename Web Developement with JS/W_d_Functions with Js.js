2d750f19037f84b9fb0ab91adfc1ef7915407531
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-13 22:35:02 +0530

    Add files via upload

---
+++ Web Developement with JS/W_d_Functions with Js.js
@@ -0,0 +1,37 @@
+console.log("This is a Tutorial 53");
+
+function greet(name, greetText = "Greetings from js..") {
+    // let name1 = Name1;  // Block level..
+    console.log(greetText + " " + name);
+    console.log(name + " is a good student");
+}
+
+function sum(a, b, c) {
+    let d = a + b + c;
+    return d;
+
+    //This line will not execute (Unreachable code)
+    //console.log("Function is not returned..");
+}
+
+let name = "Yashvi";  // Global..
+let name1 = "Sakshi";
+let name2 = "Krisha";
+let name3 = "Devanshi";
+let greetText = "Heyy Good Morning..";
+
+greet(name, greetText);
+greet(name1, greetText);
+greet(name2, greetText);
+greet(name3);
+// let returnVal = greet(name3);
+// console.log(returnVal);
+
+let returnVal = sum(10, 20, 30);
+console.log(returnVal);
+
+// fn and below syntax both are same but fn is s.c
+// console.log(name + " is a good student");
+// console.log(name1 + " is a good student");
+// console.log(name2 + " is a good student");
+// console.log(name3 + " is a good student");
